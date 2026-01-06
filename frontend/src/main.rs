cda3885b75ca44aa4b988f6834014c0b59ec34a3
Paul Zanner <go56fuq@tum.de>
2026-01-06 18:10:27 +0100

    Add Yew to frontend and write a Hello World example.

--- frontend/src/main.rs
+++ frontend/src/main.rs
@@ -1,3 +1,15 @@
+use yew::prelude::*;
+
+#[component]
+fn App() -> Html {
+    html! {
+        <>
+            <h1>{ "Finally - it works!" }</h1>
+            <p>{ "This is a showcase program presenting how Yew works." }</p>
+        </>
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    yew::Renderer::<App>::new().render();
 }
