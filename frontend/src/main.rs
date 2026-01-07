23c3b4bd74356680b403612ae982b3218015be5a
Paul Zanner <go56fuq@tum.de>
2026-01-07 18:51:45 +0100

    Switch frontend to Dioxus.
    Yew wasn't capable of carrying out sub-requests to external sources.

--- frontend/src/main.rs
+++ frontend/src/main.rs
@@ -1,42 +1,32 @@
-use std::error::Error;
-use tokio::net::TcpStream;
-use tokio::io::AsyncWriteExt;
-use yew::prelude::*;
-
-#[component]
-fn App() -> Html {
-    let rt = tokio::runtime::Runtime::new().unwrap();
-    let response_text = rt.block_on(async {
-        request_external_content().await.unwrap_or_else(|error| error.to_string())
-    });
-
-    html! {
-        <>
-            <h1>{ "Finally - it works!" }</h1>
-            <p>{ "This is a showcase program presenting how Yew works." }</p>
-            <p>{ "Let's pull something from the backend: "}{ response_text }</p>
-        </>
-    }
-}
-
-async fn request_external_content() -> Result<String, Box<dyn Error>> {
-    let address = "https://127.0.0.1:8081";
-    let resource = "/hello-world";
-
-    let url = "{address}{resource}";
-
-    let mut stream = TcpStream::connect(address).await?;
-
-    let request = format!("GET {} HTTP/3\r\nHost: {}\r\n\r\n", resource, url);
-    let request = request.as_bytes();
-
-    stream.write_all(request).await?;
-
-    let mut result: Vec<u8> = vec![];
-    stream.try_read(&mut result)?;
-    Ok(String::from_utf8(result)?)
-}
+use dioxus::prelude::*;
 
 fn main() {
-    yew::Renderer::<App>::new().render();
+    dioxus::launch(App);
+}
+
+#[component]
+fn App() -> Element {
+    let mut external_content = use_signal(|| "".to_string());
+
+    let get_external_content = move |_| async move {
+        let address = "127.0.0.1:8081";
+        let resource = "/hello-world";
+
+        let response = match reqwest::get(format!("http://{}{}", address, resource)).await {
+            Ok(response) => response
+                .text()
+                .await
+                .unwrap_or_else(|error| error.to_string()),
+            Err(error) => error.to_string(),
+        };
+
+        external_content.set(response);
+    };
+
+    rsx! {
+        h1 { "It works!" },
+        p { "Hello there!" },
+        button { onclick: get_external_content, "Load external content" },
+        p { "{external_content}" }
+    }
 }
