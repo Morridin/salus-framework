ce8a9d0d22e1ae91ee4bee30129b33161cea1ae2
Paul Zanner <go56fuq@tum.de>
2026-01-07 09:12:53 +0100

    Setup backend with a hello world example.

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{Router, routing::get};
+use std::net::SocketAddr;
+use tokio::net::TcpListener;
+
+use backend::handlers::hello_world;
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/hello-world", get(hello_world));
+
+    let addr = SocketAddr::from(([127, 0, 0, 1], 8081));
+    let listener = TcpListener::bind(addr).await.unwrap();
+
+    axum::serve(listener, app.into_make_service())
+        .await
+        .unwrap();
 }
