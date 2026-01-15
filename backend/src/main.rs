43f7fe879e3da11dc15efb5ae64d244fc2250046
Paul Zanner <go56fuq@tum.de>
2026-01-15 15:11:00 +0100

    Add SSL support for backend server.

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,17 +1,21 @@
 use axum::{Router, routing::get};
-use std::net::SocketAddr;
-use tokio::net::TcpListener;
-
+use axum_server::tls_rustls::RustlsConfig;
 use backend::handlers::hello_world;
+use std::net::SocketAddr;
 
 #[tokio::main]
 async fn main() {
     let app = Router::new().route("/hello-world", get(hello_world));
 
     let addr = SocketAddr::from(([127, 0, 0, 1], 8081));
-    let listener = TcpListener::bind(addr).await.unwrap();
 
-    axum::serve(listener, app.into_make_service())
+    let config =
+        RustlsConfig::from_pem_file("backend/.certs/cert.pem", "backend/.certs/key.pem")
+            .await
+            .unwrap();
+
+    axum_server::bind_rustls(addr, config)
+        .serve(app.into_make_service())
         .await
         .unwrap();
 }
