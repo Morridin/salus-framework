8277108744c25789dcffb75939e36562b5b89aae
Paul Zanner <go56fuq@tum.de>
2026-01-23 23:57:38 +0100

    Introduce a GUI with panels.

--- frontend/src/main.rs
+++ frontend/src/main.rs
@@ -1,14 +1,22 @@
+use crate::components::sheet::{
+    Sheet, SheetClose, SheetContent, SheetFooter, SheetHeader, SheetSide, SheetTitle,
+};
+use dioxus::html::completions::CompleteWithBraces::style;
 use dioxus::logger::tracing;
 use dioxus::prelude::*;
-use reqwest::header::{HeaderMap, ACCEPT, AUTHORIZATION, CONTENT_TYPE};
 use reqwest::Version;
+use reqwest::header::{ACCEPT, AUTHORIZATION, CONTENT_TYPE, HeaderMap};
 
+mod components;
 fn main() {
     dioxus::launch(App);
 }
 
 #[component]
 fn App() -> Element {
+    let mut closed_l = use_signal(|| "");
+    let mut closed_b = use_signal(|| "");
+    let mut closed_r = use_signal(|| "");
     let mut external_content = use_signal(|| "".to_string());
 
     let get_external_content = move |_| async move {
@@ -26,7 +34,8 @@ fn App() -> Element {
             .header(ACCEPT, "text/plain")
             .bearer_auth(token)
             .send()
-            .await {
+            .await
+        {
             Ok(response) => response
                 .text()
                 .await
@@ -38,9 +47,76 @@ fn App() -> Element {
     };
 
     rsx! {
-        h1 { "It works!" },
-        p { "Hello there!" },
-        button { onclick: get_external_content, "Load external content" },
-        p { "{external_content}" }
+        document::Stylesheet {
+            href: asset!("www-root/assets/main.css"),
+        },
+        div {
+            class: "side-panel panel {closed_l}",
+            div {
+                class: "panel-header",
+                b {
+                    "Left Panel",
+                },
+                button {
+                    class: "close-btn",
+                    onclick: move |_| closed_l.set("closed"),
+                    "X"
+                },
+            },
+            div {
+                class: "panel-body",
+                "Left panel",
+            },
+        },
+        div {
+            class: "central-pane",
+            div {
+                class: "main-panel panel",
+                div {
+                    class: "panel-body",
+                    h1 { "It works!" },
+                    p { "Hello there!" },
+                    button { onclick: get_external_content, "Load external content" },
+                    p { "{external_content}" },
+                },
+            },
+            div {
+                class: "bottom-panel panel {closed_b}",
+                div {
+                    class: "panel-header",
+                    b {
+                        "Bottom Panel",
+                    },
+                    button {
+                        class: "close-btn",
+                        onclick: move |_| closed_b.set("closed"),
+                        "X"
+                    },
+                },
+                div {
+                    class: "panel-body",
+                    "Bottom panel",
+                },
+            },
+        },
+        div {
+            class: "side-panel panel {closed_r}",
+            div {
+                class: "panel-header",
+                b {
+                    "Right Panel",
+                },
+                button {
+                    class: "close-btn",
+                    onclick: move |_| closed_r.set("closed"),
+                    "X"
+                },
+            },
+            div {
+                class: "panel-body",
+                "Right panel",
+            },
+        },
+
     }
 }
