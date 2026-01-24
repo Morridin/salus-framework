a7da9c7b6149ed8db0cd7ff5777a27187f0081ef
Paul Zanner <go56fuq@tum.de>
2026-01-24 15:12:45 +0100

    Make panels to Component for easier handling.

---
+++ frontend/src/components/panel.rs
@@ -0,0 +1,43 @@
+use dioxus::prelude::*;
+use dioxus_free_icons::Icon;
+use dioxus_free_icons::icons::vsc_icons::VscClose;
+
+/// The highest-level units the main page is built of.
+#[component]
+pub fn Panel(
+    #[props(default)]
+    class: String,
+    #[props(default)]
+    panel_name: String,
+    #[props(default = false)]
+    headless: bool,
+    children: Element,
+) -> Element {
+    let mut closed = use_signal(|| "");
+
+    rsx! {
+        div {
+            class: {format!("panel {} {}", class, closed)},
+            if !headless {
+                div {
+                    class: "panel-header",
+                    span { { panel_name }, },
+                    button {
+                        class: "close-btn",
+                        onclick: move |_| closed.set("closed"),
+                        Icon {
+                            width: 24,
+                            height: 24,
+                            fill: "black",
+                            icon: VscClose,
+                        },
+                    },
+                },
+            },
+            div {
+                class: "panel-body",
+                {children},
+            },
+        },
+    }
+}
--- frontend/src/main.rs
+++ frontend/src/main.rs
@@ -1,11 +1,9 @@
-use crate::components::sheet::{
-    Sheet, SheetClose, SheetContent, SheetFooter, SheetHeader, SheetSide, SheetTitle,
-};
 use dioxus::html::completions::CompleteWithBraces::style;
 use dioxus::logger::tracing;
 use dioxus::prelude::*;
 use reqwest::Version;
 use reqwest::header::{ACCEPT, AUTHORIZATION, CONTENT_TYPE, HeaderMap};
+use crate::components::panel::Panel;
 
 mod components;
 fn main() {
@@ -14,9 +12,6 @@ fn main() {
 
 #[component]
 fn App() -> Element {
-    let mut closed_l = use_signal(|| "");
-    let mut closed_b = use_signal(|| "");
-    let mut closed_r = use_signal(|| "");
     let mut external_content = use_signal(|| "".to_string());
 
     let get_external_content = move |_| async move {
@@ -50,73 +45,33 @@ fn App() -> Element {
         document::Stylesheet {
             href: asset!("www-root/assets/main.css"),
         },
-        div {
-            class: "side-panel panel {closed_l}",
-            div {
-                class: "panel-header",
-                b {
-                    "Left Panel",
-                },
-                button {
-                    class: "close-btn",
-                    onclick: move |_| closed_l.set("closed"),
-                    "X"
-                },
-            },
-            div {
-                class: "panel-body",
-                "Left panel",
-            },
+        Panel {
+            class: "side-panel",
+            panel_name: "Left Panel",
+            Panel {
+                class: "bottom-panel"
+            }
         },
         div {
             class: "central-pane",
-            div {
-                class: "main-panel panel",
-                div {
-                    class: "panel-body",
-                    h1 { "It works!" },
-                    p { "Hello there!" },
-                    button { onclick: get_external_content, "Load external content" },
-                    p { "{external_content}" },
-                },
+            Panel {
+                headless: true,
+                class: "main-panel",
+                h1 { "It works!" },
+                p { "Hello there!" },
+                button { onclick: get_external_content, "Load external content" },
+                p { {external_content} },
             },
-            div {
-                class: "bottom-panel panel {closed_b}",
-                div {
-                    class: "panel-header",
-                    b {
-                        "Bottom Panel",
-                    },
-                    button {
-                        class: "close-btn",
-                        onclick: move |_| closed_b.set("closed"),
-                        "X"
-                    },
-                },
-                div {
-                    class: "panel-body",
-                    "Bottom panel",
-                },
+            Panel {
+                class: "bottom-panel",
+                panel_name: "Bottom Panel",
+                "Bottom panel",
             },
         },
-        div {
-            class: "side-panel panel {closed_r}",
-            div {
-                class: "panel-header",
-                b {
-                    "Right Panel",
-                },
-                button {
-                    class: "close-btn",
-                    onclick: move |_| closed_r.set("closed"),
-                    "X"
-                },
-            },
-            div {
-                class: "panel-body",
-                "Right panel",
-            },
+        Panel {
+            class: "side-panel",
+            panel_name: "Right Panel",
+            "Right panel",
         },
-
     }
 }
