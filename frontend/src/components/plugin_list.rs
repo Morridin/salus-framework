e5b9d7c9b35db3b24b2bebbcb29298edc2996b6c
Paul Zanner <go56fuq@tum.de>
2026-02-17 20:18:28 +0100

    Add a plug-in browser prototype.
    Currently, it can only load dynamic type plug-ins correctly. We will fix this next week.

--- frontend/src/components/plugin_list.rs
+++ frontend/src/components/plugin_list.rs
@@ -1,23 +1,22 @@
 use crate::server::list_plugins;
+use dioxus::asset_resolver::AssetResolveError;
 use dioxus::prelude::*;
-use serde_json::{Value, json};
+use serde::{Deserialize, Serialize};
 
-//noinspection RsCompileErrorMacro
 #[component]
-pub fn PluginList() -> Element {
+pub fn PluginList(plugin: Signal<String>) -> Element {
     rsx! {
         h2 { "Available Plug-ins" },
         {
             let plugin_list = use_resource(move || async move {
                 if let Err(e) = list_plugins().await {
-                     return vec![format!("Error writing plugins list!\n\n{}\n", e)];
+                     return vec![format!("Error writing plug-ins list!\n\n{}\n", e)];
                 };
 
-                match dioxus::asset_resolver::read_asset_bytes(asset!("plugins/plugin-list.json")).await {
-                    Ok(bytes) => {
-                        let content = String::from_utf8_lossy(&bytes);
-                        serde_json::from_str::<Vec<String>>(&content).unwrap_or_else(|_| vec![String::from("Error parsing plug-in list!")])
-                    },
+                static LIST_ASSET: Asset = asset!("/plugins/plugin-list.json");
+
+                match dioxus::asset_resolver::read_asset_bytes(&LIST_ASSET).await {
+                    Ok(bytes) => serde_json::from_slice(&bytes).unwrap_or_else(|e| vec![format!("Error parsing plug-in list!\n\n{}\n", e)]),
                     Err(e) => vec![format!("Error loading plug-ins!\n\n{}\n", e)]
                 }
             });
@@ -28,7 +27,12 @@ pub fn PluginList() -> Element {
                 Some(plugin_list) => rsx! {
                     ul {
                         for name in plugin_list {
-                            li { "{name}" }
+                            li {
+                                PluginListEntry {
+                                    uuid: name,
+                                    plugin,
+                                }
+                            }
                         }
                     }
                 },
@@ -38,4 +42,73 @@ pub fn PluginList() -> Element {
             }
         }
     }
+}
+
+#[component]
+fn PluginListEntry(uuid: String, plugin: Signal<String>) -> Element {
+    static PLUGINS_FOLDER: Asset = asset!("/plugins");
+    let path_prototype = format!("{}/{}", PLUGINS_FOLDER, uuid);
+    let manifest = format!("{}/plugin.json", path_prototype);
+    let uuid_for_resource = uuid.clone();
+
+    let plugin_data = use_resource(move || {
+        let manifest = manifest.clone();
+        let uuid = uuid_for_resource.clone();
+        async move {
+            match dioxus::asset_resolver::read_asset_bytes(manifest).await {
+                Ok(bytes) => serde_json::from_slice(&bytes).unwrap_or_else(|e| PluginManifest {
+                    name: String::new(),
+                    kind: String::new(),
+                    source: String::new(),
+                    dependencies: vec![],
+                    panels: vec![],
+                    error: Some(format!(
+                        "Error parsing plug-in manifest for {}!\n\n{}\n",
+                        uuid, e
+                    )),
+                }),
+
+                Err(e) => PluginManifest {
+                    name: String::new(),
+                    kind: String::new(),
+                    source: String::new(),
+                    dependencies: vec![],
+                    panels: vec![],
+                    error: Some(format!(
+                        "Error loading plug-in manifest for {}!\n\n{}\n",
+                        uuid, e
+                    )),
+                },
+            }
+        }
+    });
+
+    match &*(plugin_data.read()) {
+        Some(data) => {
+            if let Some(error) = &data.error {
+                rsx! { "Error: {error}" }
+            } else {
+                let source_file = data.source.clone();
+                rsx! {
+                    span {
+                        onclick: move |_| plugin.set(format!("{}/{}", path_prototype, source_file)),
+                        "{&data.name}",
+                    },
+                }
+            }
+        },
+        None => rsx! { "Loading plug-in manifest {uuid}" },
+    }
+}
+
+#[derive(Deserialize, Serialize)]
+struct PluginManifest {
+    name: String,
+    #[serde(rename = "type")]
+    kind: String,
+    source: String,
+    dependencies: Vec<String>,
+    panels: Vec<String>,
+    #[serde(default)]
+    error: Option<String>,
 }
--- frontend/src/main.rs
+++ frontend/src/main.rs
@@ -3,6 +3,8 @@ use dioxus::prelude::*;
 use serde_json::json;
 use std::io::Write;
 use std::{fs, io};
+use wasm_bindgen::prelude::*;
+use web_sys::{MessageEvent, window};
 
 mod components;
 mod server;
@@ -13,17 +15,39 @@ fn main() {
 
 #[component]
 fn App() -> Element {
+    let mut message_origin = use_signal(|| String::from("No message received yet."));
+    let mut external_message = use_signal(|| String::from("No message received yet."));
+    let mut plugin = use_signal(|| String::new());
+
+    use_effect(move || {
+        let window = web_sys::window().expect("no global `window` exists");
+
+        let closure = Closure::wrap(Box::new(move |event: MessageEvent| {
+            let origin = event.origin();
+            message_origin.set(origin);
+
+            if let Some(data) = event.data().as_string() {
+                external_message.set(format!("Message: {data}"));
+            }
+        }) as Box<dyn FnMut(MessageEvent)>);
+
+        window
+            .add_event_listener_with_callback("message", closure.as_ref().unchecked_ref())
+            .unwrap();
+
+        closure.forget();
+    });
+
     rsx! {
         document::Stylesheet {
-            href: asset!("www-root/assets/main.css"),
+            href: asset!("/www-root/assets/main.css"),
         },
         Panel {
             class: "side-panel",
             panel_name: "Left Panel",
-            Panel {
-                class: "bottom-panel"
+            PluginList {
+                plugin
             },
-            PluginList {},
         },
         div {
             class: "central-pane",
@@ -31,13 +55,24 @@ fn App() -> Element {
                 headless: true,
                 class: "main-panel",
                 iframe {
-                    src: format!("{}/index.html", asset!("plugins/fadc"))
+                    src: plugin,
+                    //"sandbox": "allow-downloads allow-forms allow-popups allow-same-origin"
                 }
             },
             Panel {
                 class: "bottom-panel",
                 panel_name: "Bottom Panel",
-                "Bottom panel",
+                h2 { "Received messages" },
+                table {
+                    tr {
+                        th { "Message origins" },
+                        th { "Message contents" },
+                    },
+                    tr {
+                        td { "{message_origin}" },
+                        td { "{external_message}" },
+                    }
+                }
             },
         },
         Panel {
