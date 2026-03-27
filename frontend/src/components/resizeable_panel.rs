883d54f7dabd212759809b8a7a30f825616329e9
Paul Zanner <go56fuq@tum.de>
2026-03-27 23:42:01 +0100

    Fix initial panel sizing and clean resizing algorithm.

--- frontend/src/components/resizeable_panel.rs
+++ frontend/src/components/resizeable_panel.rs
@@ -1,10 +1,11 @@
-use dioxus::prelude::*;
 use crate::models::Position;
+use dioxus::prelude::*;
 
 #[component]
 pub fn ResizeablePanel(position: Position, children: Element) -> Element {
     let mut resize_active = use_signal(|| false);
-    let mut size = use_signal::<u16>(|| 288);
+    let mut size = use_signal::<i16>(|| 288);
+    let mut last_mouse_position = use_signal(|| 0.0);
 
     let (resize_horizontal, flex_direction) = match position {
         Position::Left => (true, "row"),
@@ -18,13 +19,11 @@ pub fn ResizeablePanel(position: Position, children: Element) -> Element {
             class: "panel-wrapper",
             class: if resize_horizontal { "side-panel" } else { "bottom-panel" },
             width:  match position {
-                Position::Left => format!("{size}px"),
-                Position::Right => format!("calc(100vw - {size}px)"),
+                Position::Left | Position::Right => format!("{size}px"),
                 _ => "100%".to_string(),
             },
             height: match position {
-                Position::Top => format!("{size}px"),
-                Position::Bottom => format!("calc(100vh - {size}px)"),
+                Position::Top | Position::Bottom => format!("{size}px"),
                 _ => "100%".to_string(),
             },
             flex_direction,
@@ -33,29 +32,38 @@ pub fn ResizeablePanel(position: Position, children: Element) -> Element {
                 class: "panel-resize-handler",
                 cursor: if resize_horizontal { "ew-resize" } else { "ns-resize" },
                 onmousedown: move |e: MouseEvent| {
-                    resize_active.set(true)
+                    resize_active.set(true);
+                    last_mouse_position.set(
+                        if resize_horizontal {
+                            e.coordinates().client().x
+                        }
+                        else {
+                            e.coordinates().client().y
+                        }
+                    );
                 },
                 div {
                     class: "panel-resize-overlay",
                     z_index: 2,
                     display: if *resize_active.read() { "block" } else { "none" },
                     onmousemove: move |e: MouseEvent| {
-                        if *resize_active.peek() {
-                            size.set(
-                                if resize_horizontal {
-                                     e.coordinates().client().x
-                                } else {
-                                    e.coordinates().client().y
-                                } as u16
-                            )
-                        }
+                        let current_position = if resize_horizontal {
+                            e.coordinates().client().x
+                        } else {
+                            e.coordinates().client().y
+                        };
+                        let delta = (current_position - *last_mouse_position.peek()) as i16;
+                        size += match position {
+                            Position::Left | Position::Top => delta,
+                            Position::Right | Position::Bottom => -delta,
+                        };
+                        last_mouse_position.set(current_position);
                     },
                     onmouseup: move |e: MouseEvent| {
                         resize_active.set(false);
                     },
-                }
+                },
             },
         },
     }
 }
-
